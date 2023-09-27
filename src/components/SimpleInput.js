fcef39be8a37ff8ae99ef6490e4677bd94bfaf2b
varunpats <varunpatil2502@gmail.com>
2023-09-27 21:29:34 +0530

    Receiving and logging input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('');
+
+  const onInputHandler = (event) => {
+    setName(event.target.value);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    setName('');
+  }
+
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={onInputHandler} value={name} />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button onClick={onFormSubmit}>Submit</button>
       </div>
     </form>
   );
